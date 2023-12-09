4c0c44b0c25f84edc078bc16b1dfb71c87bf9473
tandemup <ustaticdb@gmail.com>
2023-12-09 11:07:32 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const server = http.createServer();
+
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
+});
+
+const PORT = process.env.PORT || 3000;
+
+io.on("connection", (socket) => {
+  console.log("Se ha conectado un cliente");
+
+  socket.broadcast.emit("chat_message", {
+    usuario: "INFO",
+    mensaje: "Se ha conectado un nuevo usuario",
+  });
+
+  socket.on("chat_message", (data) => {
+    io.emit("chat_message", data);
+  });
+});
+
+server.listen(PORT, () => console.log(`💬 server on port ${PORT}`));
